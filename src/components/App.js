598355d98cd5aca406cc02da79db84b9169b2b37
TamasKn <tamas.knisz@gmail.com>
2019-05-17 17:32:15 +0200

    Motivation section

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import PreFooter from "./PreFooter";
 import "./App.css";
+import Motivations from './Motivations';
 
 class App extends React.Component {
   render() {
     return (
       <Fragment>
         <Navbar />
+        <Motivations />
         <PreFooter />
         <Footer />
       </Fragment>
