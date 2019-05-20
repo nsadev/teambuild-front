e12a0ce120def839d2bfbc5448af9edd88913f05
Destiny Ross <destinyross.dev@gmail.com>
2019-05-19 20:35:46 -0400

    Update UserStories.js
    Cleaned up UserStories component after moving usercomment data to a separate file

--- src/components/UserStories/UserStories.js
+++ src/components/UserStories/UserStories.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { useEffect, useState, useRef } from "react";
 import "./UserStories.css";
-import img0 from "../../static/profimg0.png";
-import img1 from "../../static/profimg1.jpeg";
-import img2 from "../../static/profimg2.jpg";
-import img3 from "../../static/profimg3.png";
-import img4 from "../../static/profimg4.jpg";
+import { UserComments } from "./UserComments";
 
 const UserStories = () => {
   // Initializes storyIndex state and setActiveStory function to update state
@@ -120,36 +116,3 @@ function useInterval(callback, delay) {
 }
 
 export default UserStories;
-
-const UserComments = [
-  {
-    userName: "Daenerys Targaryen",
-    profimg: img0,
-    userComment:
-      "Zyhys onoso jehikagon Aeksiot epi, se gis hen syndrorro jemagon. Zyhys perzys stepagon Aeksio Ono jorepi, se morghultas lys qelitsos sikagon. Vilibazmosa iderenni emilun. Valar morghulis. Skoriot nuhyz zaldrizesse ilzi?"
-  },
-  {
-    userName: "Margaery Tyrell",
-    profimg: img1,
-    userComment:
-      "In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Praesent nonummy mi in odio. Pellentesque dapibus hendrerit tortor."
-  },
-  {
-    userName: "Jon Snow",
-    profimg: img2,
-    userComment:
-      "In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Praesent nonummy mi in odio. Pellentesque dapibus hendrerit tortor."
-  },
-  {
-    userName: "Arya Stark",
-    profimg: img3,
-    userComment:
-      "In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Praesent nonummy mi in odio. Pellentesque dapibus hendrerit tortor."
-  },
-  {
-    userName: "Euron Greyjoy",
-    profimg: img4,
-    userComment:
-      "In hac habitasse platea dictumst. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Praesent nonummy mi in odio. Pellentesque dapibus hendrerit tortor."
-  }
-];
