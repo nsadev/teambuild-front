0714f9f66fd7767e67135f1c8e56a3bee78e6f95
TamasKn <tamas.knisz@gmail.com>
2019-06-06 22:42:10 +0200

    Template page added

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import "./App.css";
 import Home from "./Pages/Home";
 import Apply from "./Pages/Apply";
-import SignIn from "./Pages/SignIn/SignIn"
+import SignIn from "./Pages/SignIn/SignIn";
+
+import TemplatePage from './Pages/TemplatePage/TemplatePage';
 
 const App = () => {
 
   return (
       <BrowserRouter>
           <Route path="/" exact component={Home} />
-          <Route path="/apply" exact component={Apply} />
+          <Route path="/apply" exact component={TemplatePage} />    {/*Change for the Apply page*/}
           <Route path="/signin" exact component={SignIn} />
       </BrowserRouter>
   );
---
+++ src/components/Pages/TemplatePage/TemplatePage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './TemplatePage.css';
+import '../../../main.css'
+import Background from '../../Background/Background';
+
+
+const TemplatePage = () => {
+    return(
+        <div>
+            <Background className="background" />
+            <div className="template-container" >
+                <div className="content-box" >
+
+                    {/** Your code start below **/}
+
+
+                    <h1 className="center" >Template Page</h1>
+
+
+                    {/** Your code end above**/}
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default TemplatePage;
