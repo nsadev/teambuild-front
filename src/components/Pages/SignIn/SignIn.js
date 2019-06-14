f23ab1b1eba54c8b296b058f8134de9dd6b41cd7
StevenSymons <symons_steven@hotmail.com>
2019-06-14 16:38:50 +0200

    changed fetch url to /user/... and changed email input name to email in login component

--- src/components/Pages/SignIn/SignIn.js
+++ src/components/Pages/SignIn/SignIn.js
@@ -1,122 +1,109 @@
-import React, {useState} from 'react';
-import './SignIn.css'
-import '../../../main.css';
-import Logo from '../../Logo/Logo';
-import {Link, Redirect} from 'react-router-dom';
+import React, { useState } from "react";
+import "./SignIn.css";
+import "../../../main.css";
+import Logo from "../../Logo/Logo";
+import { Link, Redirect } from "react-router-dom";
 
+const SignIn = ({ getUser }) => {
+  const [email, setEmail] = useState(undefined);
+  const [password, setPassword] = useState(undefined);
+  const [authenticated, setAuthenticated] = useState(false);
 
-const SignIn = ({getUser}) => {
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
 
-    const [email, setEmail] = useState(undefined)
-    const [password, setPassword] = useState(undefined)
-    const [authenticated, setAuthenticated] = useState(false)
+  function handlePwChange(e) {
+    setPassword(e.target.value);
+  }
 
+  const submitSignin = () => {
+    //Add checking conditions
 
-    function handleEmailChange(e) {
-        setEmail(e.target.value)
-    }
+    fetch("/user/login", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+      .then(resp => resp.json())
+      .then(user => {
+        if (user) {
+          // State need to wait for update, otherwise user can login only for the 2nd attempt
+          setAuthenticated(true);
+          console.log(user);
+          // User object updating in App.js
+          getUser(user);
+        }
+      });
+  };
 
-    function handlePwChange(e) {
-        setPassword(e.target.value)
-    }
+  if (authenticated === true) {
+    return <Redirect to="/profile" />;
+  }
 
-    const submitSignin = () => {
+  return (
+    <div>
+      <div>
+        <div className="rectangle rectangle-1" />
+        <div className="rectangle rectangle-2" />
+        <div className="rectangle-small" />
+        <div className="rectangle rectangle-3" />
+        <div className="rectangle rectangle-4" />
+      </div>
 
-        //Add checking conditions
-
-        fetch('http://localhost:5000/user/login', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                email: email,
-                password: password
-            })
-        }).then(resp => resp.json())
-            .then(user => {
-                if(user){
-                    // State need to wait for update, otherwise user can login only for the 2nd attempt
-                    setTimeout(() => {setAuthenticated(true)}, 1)
-
-                    // User object updating in App.js
-                    getUser(user)
-                }
-            })
-    }
-
-
-    if(authenticated === true){
-        return <Redirect to='/profile' />
-    }
-
-    return(
-        <div>
-
-
-            <div>
-                <div className="rectangle rectangle-1" />
-                <div className="rectangle rectangle-2" />
-                <div className="rectangle-small" />
-                <div className="rectangle rectangle-3" />
-                <div className="rectangle rectangle-4" />
-            </div>
-
-            <div className="navbar">
-                <div className="container">
-                    <div className="navbar__logo">
-                        <Logo/>
-                    </div>
-                </div>
-            </div>
-
-            <div className="template-container signin-container">
-
-                <div className="signin-window">
-
-                    <h1 className="center form-text-color">Sign In</h1>
-
-                    <form className="signin-form">
-                        <label className="form-text-color form-text">E-mail</label>
-                        <input className="signin-input form-text-color"
-                               type="email"
-                               name="username"
-                               onChange={handleEmailChange}
-                        />
-
-                        <label className="form-text form-text-color">Password</label>
-                        <input className="signin-input form-text-color"
-                               type="password"
-                               name="password"
-                               onChange={handlePwChange}
-                        />
-
-                        <div className="signin-button center">
-                            <a className="cta-button-form" onClick={submitSignin} >
-                                Sign In
-                            </a>
-                        </div>
-
-                    </form>
-
-                    <div className="center form-text-color signin-footer">
-                        Don't have an account? <Link className="signin-footer" to="/apply"><a href="#">Apply</a></Link>
-
-
-                        {/*Delete when auth logic done*/}
-                            <br/><Link to="/register" >Register</Link>
-
-                        {/*--------------------------------------*/}
-
-                    </div>
-
-                </div>
-
-            </div>
-
+      <div className="navbar">
+        <div className="container">
+          <div className="navbar__logo">
+            <Logo />
+          </div>
         </div>
+      </div>
 
-    )
-}
+      <div className="template-container signin-container">
+        <div className="signin-window">
+          <h1 className="center form-text-color">Sign In</h1>
 
+          <form className="signin-form">
+            <label className="form-text-color form-text">E-mail</label>
+            <input
+              className="signin-input form-text-color"
+              type="email"
+              name="email"
+              onChange={handleEmailChange}
+            />
 
+            <label className="form-text form-text-color">Password</label>
+            <input
+              className="signin-input form-text-color"
+              type="password"
+              name="password"
+              onChange={handlePwChange}
+            />
+
+            <div className="signin-button center">
+              <a className="cta-button-form" onClick={submitSignin}>
+                Sign In
+              </a>
+            </div>
+          </form>
+
+          <div className="center form-text-color signin-footer">
+            Don't have an account?{" "}
+            <Link className="signin-footer" to="/apply">
+              <a href="#">Apply</a>
+            </Link>
+            {/*Delete when auth logic done*/}
+            <br />
+            <Link to="/register">Register</Link>
+            {/*--------------------------------------*/}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SignIn;
