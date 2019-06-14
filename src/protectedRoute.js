5c22496701515038e9567c4a7124b49ac8d65a84
StevenSymons <symons_steven@hotmail.com>
2019-06-14 22:11:24 +0200

    imported auth util into protectedRoute

--- src/protectedRoute.js
+++ src/protectedRoute.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
+import auth from "../utils/Auth";
 
 export const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
