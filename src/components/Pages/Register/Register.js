324fcb30ff7f18e71f854217c52780b71728dbac
StevenSymons <symons_steven@hotmail.com>
2019-06-15 13:10:16 +0200

    resolved last merge problem at register

--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -6,7 +6,6 @@ import Background from "../../Background/Background";
 import { Link } from "react-router-dom";
 
 const Register = () => {
-<<<<<<< HEAD
   const [email, setEmail] = useState(null);
   const [firstname, setFirstname] = useState(null);
   const [lastname, setLastname] = useState(null);
@@ -126,119 +125,6 @@ const Register = () => {
               >
                 Register
               </a>
-=======
-
-    const [email, setEmail] = useState(null)
-    const [firstname, setFirstname] = useState(null)
-    const [lastname, setLastname] = useState(null)
-    const [password, setPassword] = useState(null)
-    const [confPw, setConfPw] = useState(null)
-    const [message, setMessage] = useState(null)
-
-    function emailChange(e){
-        setEmail(e.target.value)
-    }
-
-    function firstnameChange(e){
-        setFirstname(e.target.value)
-    }
-
-    function lastnameChange(e){
-        setLastname(e.target.value)
-    }
-
-    function passwordChange(e){
-        setPassword(e.target.value)
-    }
-
-    function confPwChange(e){
-        setConfPw(e.target.value)
-    }
-
-
-    const onSubmitRegister = () => {
-
-        //add feedback messages
-
-        if(email && firstname && lastname && password && confPw){
-            if(password.length >= 8){
-                if(password === confPw){
-
-                    fetch('http://localhost:5000/user/register', {
-                        method: 'post',
-                        headers: {'Content-Type': 'application/json'},
-                        body: JSON.stringify({
-                            email: email,
-                            first_name: firstname,
-                            last_name: lastname,
-                            password: password
-                        })
-                    }).then(resp => resp.json())
-                        .then(user => {
-                            if(user) {
-                                //redirect to the profile page
-                                setMessage(user.message)
-
-
-                            }
-                        })
-                } else {
-                    setMessage('Passwords are not matching')
-                }
-            } else {
-                setMessage('Password is too short')
-            }
-        } else {
-            setMessage("Every fields are mandatory")
-        }
-
-
-    }
-
-    return(
-        <div>
-            <Background className="background" />
-            <div className="template-container signin-container" >
-
-                    <div className="register-window">
-
-                        <h1 className="center form-text-color">Register</h1>
-
-                        <form className="signin-form">
-                            <label className="form-text-color form-text">E-mail</label>
-                            <input className="signin-input form-text-color" type="email"
-                                   name="e-mail" id="email" onChange={emailChange}/>
-
-                            <label className="form-text-color form-text" >First name</label>
-                            <input className="signin-input form-text-color" type="text"
-                                   name="firstname" id="firstname" onChange={firstnameChange}/>
-
-                            <label className="form-text-color form-text" >Last name</label>
-                            <input className="signin-input form-text-color" type="text"
-                                   name="lastname" id="lastname" onChange={lastnameChange}/>
-
-                            <label className="form-text-color form-text" >
-                                Password <ins className="field-info">minimum 8 characters</ins>
-                            </label>
-                            <input className="signin-input form-text-color" type="password"
-                                   name="password" id="password" onChange={passwordChange}/>
-
-                            <label className="form-text-color form-text" >Confirm password</label>
-                            <input className="signin-input form-text-color" type="password"
-                                   name="confPw" id="confPw" onChange={confPwChange}/>
-
-                            <div className="error-msg center"><p>{message}</p></div>
-                            <div className="signin-button center">
-                                <a className="cta-button-form" type="submit" onClick={onSubmitRegister} >
-                                    Register
-                                </a>
-                            </div>
-
-                        </form>
-
-                    </div>
-
->>>>>>> fc3f188cb5093ff0dde41a049340524e7cbee50b
             </div>
           </form>
         </div>
