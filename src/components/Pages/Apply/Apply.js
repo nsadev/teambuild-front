7e0c9e5828b9e563d27bff2ea8fa28e4523e71c2
TamasKn <tamas.knisz@gmail.com>
2019-06-18 19:22:02 +0200

    Application form updated

--- src/components/Pages/Apply/Apply.js
+++ src/components/Pages/Apply/Apply.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react"
-import "./Apply.css"
 import "../../../main.css"
-import Logo from "../../Logo/Logo"
-import { Link } from "react-router-dom"
+import "./Apply.css"
+import "../SignIn/SignIn.css"
+
+
 import Background from "../../Background/Background"
 
 const Apply = props => {
-    const [email, setEmail] = useState("")
-    const [name, setName] = useState("")
-    const [textArea, setTextArea] = useState("")
-    const [githubName, setGithubName] = useState("")
+    const [email, setEmail] = useState(null)
+    const [name, setName] = useState(null)
+    const [textArea, setTextArea] = useState(null)
+    const [githubName, setGithubName] = useState(null)
 
     function onEmailChange(e) {
         setEmail(e.target.value)
@@ -46,14 +47,14 @@ const Apply = props => {
 
             <div className="template-container apply-container">
                 <div className="apply-window">
-                    <h1 className="center form-text-color">Apply</h1>
+                    <h1 className="center form-text-color">Application</h1>
 
                     <form className="apply-form">
                         <label className="form-text-color form-text">
                             Name
                         </label>
                         <input
-                            className="apply-input form-text-color"
+                            className="signin-input apply-input form-text-color"
                             type="name"
                             name="name"
                             onChange={onNameChange}
@@ -63,7 +64,7 @@ const Apply = props => {
                             E-mail
                         </label>
                         <input
-                            className="apply-input form-text-color"
+                            className="signin-input apply-input form-text-color"
                             type="email"
                             name="email"
                             onChange={onEmailChange}
@@ -72,18 +73,39 @@ const Apply = props => {
                             GitHub Account
                         </label>
                         <input
-                            className="apply-input form-text-color"
+                            className="signin-input apply-input form-text-color"
                             type="text"
                             name="github-account-name"
                             onChange={onGithubNameChange}
                         />
-                        <label className="label-text-area">
-                            Tell us about yourself, tech used etc.
+
+                        <label className="form-text form-text-color">
+                            What developer role do you prefer?
+                        </label>
+                        <div className="radio-buttons form-text-color" >
+                            <input type="radio" value="front-design" name="stack"/>Front-End/Design
+                            <input type="radio" value="back" name="stack"/>Back-End
+                            <input type="radio" value="full" name="stack"/>Full-Stack
+                        </div>
+
+                        <label className="form-text form-text-color">
+                            Tech Stacks you are familiar with
                         </label>
                         <textarea
-                            className="text-area"
+                            className="signin-input apply-input text-area form-text-color"
                             type="text"
-                            name="github-account-name"
+                            name="techStack"
+                            placeholder="Ex.: Javascript, Node.js etc."
+                            onChange={onTextChange}
+                        />
+
+                        <label className="form-text form-text-color">
+                            Why do you want to join TeamBuild?
+                        </label>
+                        <textarea
+                            className="signin-input apply-input signin-input text-area form-text-color"
+                            type="text"
+                            name="joinReason"
                             onChange={onTextChange}
                         />
 
