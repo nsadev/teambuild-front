badd9f67bc0a739c784d764cc61d59051cd8e4ef
Chandler Baskins <baskinschandler@gmail.com>
2019-06-24 23:36:29 -0500

    conditionally renders success message

--- src/components/Pages/Apply/Apply.js
+++ src/components/Pages/Apply/Apply.js
@@ -14,6 +14,9 @@ const Apply = props => {
     const [stack, setStack] = useState(null)
     const [joinReason, setJoinReason] = useState(null)
     const [message, setMessage] = useState(null)
+    const [success, setSuccess] = useState(false)
+    
+    
 
 
     function onEmailChange(e) {
@@ -40,7 +43,10 @@ const Apply = props => {
         setJoinReason(e.target.value)
     }
 
-    console.log(name, email, githubName, role, stack, joinReason)
+
+    const Success = () => (
+        <h3>Success!!!!!!!!!!</h3>
+    );
 
     function onSubmit() {
 
@@ -58,108 +64,125 @@ const Apply = props => {
                         joinReason: joinReason
                     }),
                 }).then(response => response.json())
+                    setSuccess(true)
                     .then(data => {
                         setMessage(data.message)
                     })
 
-            } catch (e) {
+            }
+            
+            catch (e) {
                 setMessage(e)
             }
-
+            
         } else {
             setMessage("Every fields are mandatory")
         }
 
-
+      console.log(message)
     }
 
-    return (
-        <div>
-            <Background />
+    if(success ) {
+        return (
+            <Success/>
+        );
+    } else {
+        return (
+    <div>
+        <Background />
 
-            <div className="template-container apply-container">
-                <div className="apply-window">
-                    <h1 className="center form-text-color">Application</h1>
+        <div className="template-container apply-container">
+            <div className="apply-window">
+                <h1 className="center form-text-color">Application</h1>
 
-                    <form className="apply-form">
-                        <label className="form-text-color form-text">
-                            Name
-                        </label>
-                        <input
-                            className="signin-input apply-input form-text-color"
-                            type="name"
-                            name="name"
-                            onChange={onNameChange}
-                        />
+                <form className="apply-form">
+                    <label className="form-text-color form-text">
+                        Name
+                    </label>
+                    <input
+                        className="signin-input apply-input form-text-color"
+                        type="name"
+                        name="name"
+                        onChange={onNameChange}
+                    />
 
-                        <label className="form-text form-text-color">
-                            E-mail
-                        </label>
-                        <input
-                            className="signin-input apply-input form-text-color"
-                            type="email"
-                            name="email"
-                            onChange={onEmailChange}
-                        />
-                        <label className="form-text form-text-color">
-                            GitHub Account
-                        </label>
-                        <input
-                            className="signin-input apply-input form-text-color"
-                            type="text"
-                            name="github-account-name"
-                            onChange={onGithubNameChange}
-                        />
+                    <label className="form-text form-text-color">
+                        E-mail
+                    </label>
+                    <input
+                        className="signin-input apply-input form-text-color"
+                        type="email"
+                        name="email"
+                        onChange={onEmailChange}
+                    />
+                    <label className="form-text form-text-color">
+                        GitHub Account
+                    </label>
+                    <input
+                        className="signin-input apply-input form-text-color"
+                        type="text"
+                        name="github-account-name"
+                        onChange={onGithubNameChange}
+                    />
 
-                        <label className="form-text form-text-color">
-                            What developer role do you prefer?
-                        </label>
-                        <div className="radio-buttons form-text-color" >
-                            <input type="radio" value="Front-End/Design" name="stack"
-                                   onChange={roleSelection} />Front-End/Design
-                            <input type="radio" value="Back-End" name="stack"
-                                   onChange={roleSelection} />Back-End
-                            <input type="radio" value="Full-Stack" name="stack"
-                                   onChange={roleSelection} />Full-Stack
-                        </div>
+                    <label className="form-text form-text-color">
+                        What developer role do you prefer?
+                    </label>
+                    <div className="radio-buttons form-text-color" >
+                        <input type="radio" value="Front-End/Design" name="stack"
+                               onChange={roleSelection} />Front-End/Design
+                        <input type="radio" value="Back-End" name="stack"
+                               onChange={roleSelection} />Back-End
+                        <input type="radio" value="Full-Stack" name="stack"
+                               onChange={roleSelection} />Full-Stack
+                    </div>
 
-                        <label className="form-text form-text-color">
-                            Technologies you are familiar with
-                        </label>
-                        <textarea
-                            className="signin-input apply-input text-area form-text-color"
-                            type="text"
-                            name="techStack"
-                            placeholder="Javascript, Node.js etc."
-                            onChange={onStackChange}
-                        />
+                    <label className="form-text form-text-color">
+                        Technologies you are familiar with
+                    </label>
+                    <textarea
+                        className="signin-input apply-input text-area form-text-color"
+                        type="text"
+                        name="techStack"
+                        placeholder="Javascript, Node.js etc."
+                        onChange={onStackChange}
+                    />
 
-                        <label className="form-text form-text-color">
-                            Why do you want to join TeamBuild?
-                        </label>
-                        <textarea
-                            className="signin-input apply-input signin-input text-area form-text-color"
-                            type="text"
-                            name="joinReason"
-                            onChange={onJoinChange}
-                        />
+                    <label className="form-text form-text-color">
+                        Why do you want to join TeamBuild?
+                    </label>
+                    <textarea
+                        className="signin-input apply-input signin-input text-area form-text-color"
+                        type="text"
+                        name="joinReason"
+                        onChange={onJoinChange}
+                    />
 
-                        <div className="error-msg center"><p>{message}</p></div>
+                    <div className="error-msg center"><p>{message}</p></div>
 
-                        <div className="apply-button center">
-                            <a
-                                className="cta-button-form"
-                                type="submit"
-                                onClick={onSubmit}
-                            >
-                                Submit
-                            </a>
-                        </div>
-                    </form>
-                </div>
+                    <div className="apply-button center">
+                        <a
+                            className="cta-button-form"
+                            type="submit"
+                            onClick={onSubmit}
+                        >
+                            Submit
+                        </a>
+                    </div>
+                </form>
             </div>
         </div>
-    )
+    </div>
+)
+    }
 }
 
+
+
+
+
+
 export default Apply
+
+
+
