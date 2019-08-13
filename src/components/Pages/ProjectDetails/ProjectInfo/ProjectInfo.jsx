36be0993ad18422d5af40c5d1271c24bdee25fe4
Teva Henry <35941084+TevaHenry@users.noreply.github.com>
2019-08-13 23:13:45 +1200

    Finalises ProjectInfo

--- src/components/Pages/ProjectDetails/ProjectInfo/ProjectInfo.jsx
+++ src/components/Pages/ProjectDetails/ProjectInfo/ProjectInfo.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import Git from './github.png';
+import GitIcon from '../../../../static/github_icon.png';
 import './ProjectInfo.css';
 
-const ProjectInfo = (props) => (
-  <div className='content'>
-    <div className="project-info">
-      <div className="name-and-link">
-        <h2 className="project-name">{props.name}</h2>
-        <a href="#"><img src={Git} alt="github" className='git-link'/></a>
+const ProjectInfo = () => {
+  const project = {
+    name: "Project Name",
+    github: '#',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem repellendus eligendi veritatis repellat. Commodi magni voluptates neque ab saepe, quos sequi, sed corporis quisquam a cumque iste veniam quae fugit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis id cum exercitationem, iusto mollitia repudiandae incidunt commodi eum reiciendis? Blanditiis, consequuntur explicabo dolor hic est animi dolore facilis ipsum quae. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat, maxime amet! Modi architecto nisi unde molestias, explicabo temporibus consequatur similique, non sed in ullam debitis ut incidunt nemo, nesciunt quisquam?',
+    thumbnail: '#'
+  }
+
+  return (
+    <div className='content'>
+      <div className="project-info">
+        <div className="name-and-link">
+          <h2 className="project-name">{project.name}</h2>
+          <a href={project.github}><img src={GitIcon} alt="github" className='git-link'/></a>
+        </div>
+        <div className="info">
+          <p>{project.description}</p>
+        </div>
       </div>
-      <div className="info">
-        <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat accusantium, excepturi blanditiis amet, quidem harum nisi numquam totam eaque labore aut cumque at ipsa animi doloremque laudantium, nulla natus facere! lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe distinctio minima quasi! Doloribus voluptas consequuntur numquam corporis et, itaque neque ducimus inventore explicabo culpa molestiae commodi excepturi fugit velit architecto!
-        </p>
+      <div className="project-thumbnail">
+        <img src={project.thumbnail} alt="project"/>
       </div>
     </div>
-    <div className="project-thumbnail">
-      <img src="#" alt="project"/>
-    </div>
-  </div>
-)
+  )
+}
 
 export default ProjectInfo;
